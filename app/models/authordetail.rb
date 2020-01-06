4ab4e5860e98161796bb3680a5f2147222b37a8d
webcontive <wc.bhadresh@gmail.com>
2020-01-06 10:07:32 +0530

    validates authordetails

--- app/models/authordetail.rb
+++ app/models/authordetail.rb
@@ -2,6 +2,15 @@ class Authordetail < ApplicationRecord
 #-----------------------------
 	belongs_to :author#succeessfull                                           
 #------------------------------
+validates :email,  
+            presence: true,  
+            uniqueness: true,  
+            format: {  
+              with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i  
+            }
+
+validates :bio, presence: true
+validates :mobile_No, presence: true
 
 end
 
